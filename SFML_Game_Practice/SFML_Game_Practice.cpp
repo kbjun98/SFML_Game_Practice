a550ce7df79be409c74a1b9aa8d68fb1d4d613b5
kbjun98 <kbjun98@gmail.com>
2022-08-10 15:40:37 +0900

    Open window and draw background

--- SFML_Game_Practice/SFML_Game_Practice.cpp
+++ SFML_Game_Practice/SFML_Game_Practice.cpp
@@ -1,20 +1,25 @@
-// SFML_Game_Practice.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+#include <SFML/Graphics.hpp>
+using namespace sf;
 int main()
 {
-    std::cout << "Hello World!\n";
+    VideoMode vm(1920, 1080);
+    RenderWindow window(vm, "Timber", Style::Fullscreen);
+    Texture textureBackground;
+    textureBackground.loadFromFile("Graphics/background.png");
+    Sprite spriteBackground;
+    spriteBackground.setTexture(textureBackground);
+    spriteBackground.setPosition(0, 0);
+    while (window.isOpen())
+    {
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();
+        }
+        window.clear();
+        window.draw(spriteBackground);
+        window.display();
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
